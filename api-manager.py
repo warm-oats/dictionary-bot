4e8b2076a58fa4d849e22afd0430b3cf614803f8
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-20 00:39:57 -0700

    Create parent class APIManager for word api managers

--- api-manager.py
+++ api-manager.py
@@ -2,21 +2,43 @@ import os
 import requests
 from dotenv import load_dotenv
 
-class APIManager:
+class WordAPIManager:
+    URL = None
+
+    def __init__(self):
+        pass
+
+    def request_word(self):
+        dict_response = requests.get(eval(f'f"{self.URL}"')) 
+
+        return dict_response
+
+class RandomAPIManager(WordAPIManager):
+    def __init__(self):
+        super()
+
+    def request_word(self, word):
+        dict_response = super().request_word(word)
+
+class MerriamAPIManager(WordAPIManager):
 
     load_dotenv()
 
     DICT_API = os.getenv('DICTIONARY_API')
+    URL = "https://dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={MerriamAPIManager.DICT_API}"
 
     def __init__(self):
+        super()
         load_dotenv()
 
     def request_word(self, word):
-        api_request = requests.get(f"https://dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={APIManager.DICT_API}")
-        
-        print(api_request.json()[0])
+        dict_response = super().request_word(word)
+        word_name = dict_response.json()[0]["meta"]["id"].lower().strip()
+        stem_set = set(map(lambda stem: stem.split(" ")[0], dict_response.json()[0]["meta"]["stems"]))
+
+        print(dict_response.json())
 
 if __name__ == "__main__":
-    api_manager = APIManager()
+    api_manager = MerriamAPIManager()
 
-    api_manager.request_word('hello')
+    api_manager.request_word('voluminous')
