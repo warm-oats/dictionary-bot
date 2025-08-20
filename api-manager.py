89f61102b644c1db6fd15667ffbb70c6e80bfa4e
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-20 10:53:33 -0700

    Revamp RandomAPIManager and MerriamAPIManager

--- api-manager.py
+++ api-manager.py
@@ -9,7 +9,8 @@ class RandomAPIManager():
         super()
 
     def request(self, amount=1, word_length=4):
-        dict_response = requests.get(f"https://random-word-api.herokuapp.com/word?number={amount}?length={word_length}")
+        dict_response = requests.get(f"https://random-word-api.herokuapp.com/word?number={amount}&length={word_length}")
+        words = dict_response.json() # Output is a list
 
 class MerriamAPIManager():
 
@@ -26,9 +27,5 @@ class MerriamAPIManager():
         word_name = dict_response.json()[0]["meta"]["id"].lower().strip()
         stem_set = set(map(lambda stem: stem.split(" ")[0], dict_response.json()[0]["meta"]["stems"]))
 
-        print(dict_response.json())
-
 if __name__ == "__main__":
-    api_manager = MerriamAPIManager()
-
-    api_manager.request('voluminous')
+    pass
