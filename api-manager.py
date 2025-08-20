3d73472f087ec180e8cf578aaac14fb3cf021113
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-20 00:45:25 -0700

    Change api call function name

--- api-manager.py
+++ api-manager.py
@@ -3,22 +3,26 @@ import requests
 from dotenv import load_dotenv
 
 class WordAPIManager:
+
     URL = None
 
     def __init__(self):
         pass
 
-    def request_word(self):
+    def request(self):
         dict_response = requests.get(eval(f'f"{self.URL}"')) 
 
         return dict_response
 
 class RandomAPIManager(WordAPIManager):
+
+    URL = "https://random-word-api.herokuapp.com/word?number={amount}?length={word_length}"
+
     def __init__(self):
         super()
 
-    def request_word(self, word):
-        dict_response = super().request_word(word)
+    def request(self, amount=1, word_length=4):
+        dict_response = super().request_word()
 
 class MerriamAPIManager(WordAPIManager):
 
@@ -31,7 +35,7 @@ class MerriamAPIManager(WordAPIManager):
         super()
         load_dotenv()
 
-    def request_word(self, word):
+    def request(self, word):
         dict_response = super().request_word(word)
         word_name = dict_response.json()[0]["meta"]["id"].lower().strip()
         stem_set = set(map(lambda stem: stem.split(" ")[0], dict_response.json()[0]["meta"]["stems"]))
@@ -41,4 +45,4 @@ class MerriamAPIManager(WordAPIManager):
 if __name__ == "__main__":
     api_manager = MerriamAPIManager()
 
-    api_manager.request_word('voluminous')
+    api_manager.request('voluminous')
