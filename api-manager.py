3fd1d5408aa41d25b02bfad0f31b8e1ee44b7f09
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-19 23:41:47 -0700

    Add venv environment, create API Manager

--- api-manager.py
+++ api-manager.py
@@ -3,5 +3,20 @@ import requests
 from dotenv import load_dotenv
 
 class APIManager:
+
+    load_dotenv()
+
+    DICT_API = os.getenv('DICTIONARY_API')
+
     def __init__(self):
-        pass
+        load_dotenv()
+
+    def request_word(self, word):
+        api_request = requests.get(f"https://dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={APIManager.DICT_API}")
+        
+        print(api_request.json()[0])
+
+if __name__ == "__main__":
+    api_manager = APIManager()
+
+    api_manager.request_word('hello')
