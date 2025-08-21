de16fc47074fbacb7afc3efc45c225de2453cad7
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-20 17:26:28 -0700

    Add PapagoAPIManager and function to req. translation

--- src/api_manager.py
+++ src/api_manager.py
@@ -1,13 +1,27 @@
 import os
 import requests
 import asyncio
+from googletrans import Translator as GoogleTranslator
 from dotenv import load_dotenv
 
 # f-strings make OOP for this very hard, so using separate classes
 
-class PapagoKoreanAPIManager():
-    def __init__(self):
-        pass
+class PapagoAPIManager():
+
+    google_translator = GoogleTranslator()
+
+    def translate_word(self, word):
+        async def sync_translate():
+            async with self.google_translator:
+                #result = await google_translator.translate('안녕하세요.')
+                #print(result)  # <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
+                    
+                return await self.google_translator.translate(word, dest='ko')
+        
+                #result = await google_translator.translate('veritas lux mea', src='la')
+                #print(result)  # <Translated src=la dest=en text=The truth is my light pronunciation=The truth is my light>
+
+        return asyncio.run(sync_translate())
 
 class RandomAPIManager():
     def __init__(self):
@@ -33,8 +47,6 @@ class MerriamAPIManager():
         return dict_response.json()
 
 if __name__ == "__main__":
-    random = RandomAPIManager()
-    merriam = MerriamAPIManager()
+    papago = PapagoAPIManager()
 
-    random.request_json()
-    merriam.request_json('hey')
+    print(papago.translate_word('get'))
