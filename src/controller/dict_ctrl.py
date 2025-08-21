9460494a55ef7d2349d702ede4d14a0696337834
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-21 00:48:14 -0700

    Add additional layers to model layer
    Model layer now includes an API manager module to call
    APIs and the domain logic module to process API
    data.

--- src/controller/dict_ctrl.py
+++ src/controller/dict_ctrl.py
@@ -2,20 +2,23 @@ from discord.ext import commands
 from pathlib import Path
 import sys
 sys.path.insert(0, str(Path(__file__).parent.parent))
+from model.dict_model.dict_model import DictModel
+from view.dict_view import DictView
 
 class DictController(commands.Cog):
 
-    
+    dict_model = DictModel()
+    dict_view = DictView()
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name = "defineword")
     async def define_word(self, ctx, word):
-        word_info = self.translator.get_word(word)
+        word_info = self.dict_model.get_word(word)
 
         await ctx.channel.send(word_info['word_name'])
 
 async def setup(bot):
-    print("Inside word dictionary setup function")
+    print("Inside dictionary controller setup function")
     await bot.add_cog(DictController(bot))
--- src/view/dict_view.py
+++ src/view/dict_view.py
@@ -3,5 +3,3 @@ from discord.ext import commands
 class DictView:
     def __init__(self):
         pass
-
-    def view_word(self, )
