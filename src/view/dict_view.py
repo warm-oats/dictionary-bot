4652fed092910f38d12b78bbdd99cb5c84ff2640
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-21 17:49:55 -0700

    Add embed function for dict view layer

--- src/view/dict_view.py
+++ src/view/dict_view.py
@@ -1,6 +1,22 @@
+import discord
+
 class DictView:
     def __init__(self):
         pass
     
-    async def post_word_info(self, ctx, word):
-        await ctx.channel.send(word)
+    async def post_word_info(self, ctx, word_info):
+        embed = self.create_embed(word_info)
+        await ctx.channel.send(embed = embed)
+
+    def create_embed(self, word_info):
+        embed = discord.Embed(title = word_info["word_name"].capitalize())
+
+        for count, definition in enumerate(word_info["definitions"], start = 1):
+            embed.add_field(name = f"Definition {count}", value = definition.capitalize(), inline = False)
+
+        embed.add_field(name = "Stem Set", value =  " ".join(word_info["stem_set"]).capitalize(), inline = False)
+        embed.add_field(name = "Part of Speech", value = word_info["part_of_speech"].capitalize(), inline = False)
+
+        return embed
+
+        
