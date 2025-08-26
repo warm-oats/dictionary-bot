417fc5f23ee0c2776d5ec738c1de24f445a0c5c8
warm-oats <61860115+warm-oats@users.noreply.github.com>
2025-08-26 14:41:00 -0700

    Add index validation for def contexts

--- src/controller/dict_ctrl.py
+++ src/controller/dict_ctrl.py
@@ -17,21 +17,29 @@ class DictController(commands.Cog):
     @commands.command(name = "define")
     async def async_define_word(self, ctx, word):
         def_contexts = DictController.dict_model.get_word_info(word) 
-        button_controller = ButtonController()
-        await DictController.dict_view.post_word_info(ctx, def_contexts, ButtonController())
+        button_controller = ButtonController(def_contexts)
+        await DictController.dict_view.post_word_info(ctx, def_contexts, button_controller)
 
 class ButtonController(discord.ui.View):
-    def __init__(self):
+    def __init__(self, def_contexts):
         super().__init__()
         self.value = None
+        self.def_contexts = def_contexts
+        self.def_context_i = 0
 
     @discord.ui.button(label="<<", style=discord.ButtonStyle.blurple)
     async def prev_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('CLICKED PREV!')
+        await interaction.response.edit_message('CLICKED PREV!')
 
     @discord.ui.button(label=">>", style=discord.ButtonStyle.blurple)
     async def next_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('CLICKED NEXT!')
+        await interaction.response.edit_message('CLICKED NEXT!')
+
+    def is_valid_index(self, index, lst_len):
+        if index < 0 or index >= lst_len:
+            return False
+        
+        return True
 
 async def setup(bot):
     print("Inside dict controller setup function")
