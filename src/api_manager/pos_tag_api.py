ec568148492ae1df76c8eee179e37b77fa8cf860
Quan Huynh <bronce7274@berkeley.edu>
2025-10-01 09:24:48 -0700

    Use OKT as POS tagger

--- src/api_manager/pos_tag_api.py
+++ src/api_manager/pos_tag_api.py
@@ -1,12 +1,24 @@
-from konlpy.tag import Mecab
+from konlpy.tag import Okt
 
 class POSTagAPIManager():
 
     def extract_pos(self, phrase):
-        return Mecab().pos(phrase)
+        okt = Okt()
+        pos_map = {'Noun': [], 'Verb': [], 'Adjective': []}
+
+        processed_phrase = okt.pos(phrase, norm = True, stem = True)
+
+        for word_tup in processed_phrase:
+            POS_i = 1
+            WORD_i = 0
+
+            if (word_tup[POS_i] in pos_map):
+                pos_map[word_tup[POS_i]] = word_tup[WORD_i]
+
+        print(pos_map)
     
 if __name__ == '__main__':
 
     mecab = POSTagAPIManager()
 
-    print(mecab.extract_pos("안녕하세요"))
+    print(mecab.extract_pos("달은 정말 아름답습니다"))
