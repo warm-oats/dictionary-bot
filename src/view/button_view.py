7b627cffa024f431d2f033d81364470468f3880e
Quan Huynh <bronce7274@berkeley.edu>
2025-10-01 20:51:18 -0700

    Add custom button class

--- src/view/button_view.py
+++ src/view/button_view.py
@@ -1,5 +1,21 @@
 import discord
 
+class CustomButton(discord.ui.Button):
+
+    def __init__(self, callback_func, style, label, disabled = False, custom_id = None, url = None, emoji = None, row = None, sku_id = None, id = None):
+        super().__init__(
+            label = label, 
+            style = style, 
+            disabled = disabled, 
+            custom_id = custom_id, 
+            url = url, 
+            emoji = emoji,
+            row = row,
+            sku_id = sku_id,
+            id = id
+        )
+        self.callback = callback_func
+
 class ButtonView(discord.ui.View):
 
     def __init__(self, contexts, edit_func, context_i = 0, context_num = 1):
@@ -8,8 +24,10 @@ class ButtonView(discord.ui.View):
         self.edit_func = edit_func
         self.context_i = context_i
         self.context_num = context_num
+        self.test_button = CustomButton(self.next_button, label="Test", style = discord.ButtonStyle.blurple)
 
-    @discord.ui.button(label = "<<", style = discord.ButtonStyle.blurple)
+        self.add_item(item = self.test_button)
+
     async def prev_button(self, interaction: discord.Interaction, button: discord.ui.Button):
 
         contexts_len = len(self.contexts)
@@ -22,8 +40,7 @@ class ButtonView(discord.ui.View):
 
             self.context_i -= 1
 
-    @discord.ui.button(label = ">>", style = discord.ButtonStyle.blurple)
-    async def next_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def next_button(self, interaction: discord.Interaction):
 
         contexts_len = len(self.contexts)
 
