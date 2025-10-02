3bae430eafb52862c4120e7bd9c42d9c75eb14cc
Quan Huynh <bronce7274@berkeley.edu>
2025-10-01 21:37:38 -0700

    Set up kwargs for arbitrary function parameters

--- src/view/button_view.py
+++ src/view/button_view.py
@@ -2,19 +2,16 @@ import discord
 
 class CustomButton(discord.ui.Button):
 
-    def __init__(self, callback_func, style, label, disabled = False, custom_id = None, url = None, emoji = None, row = None, sku_id = None, id = None):
-        super().__init__(
-            label = label, 
-            style = style, 
-            disabled = disabled, 
-            custom_id = custom_id, 
-            url = url, 
-            emoji = emoji,
-            row = row,
-            sku_id = sku_id,
-            id = id
-        )
-        self.callback = callback_func
+    def __init__(self, callback_func, style, label, **callback_params):
+        super().__init__(label = label, style = style)
+        self.callback_func = callback_func
+        self.callback_params = callback_params
+
+    async def callback(self, interaction):
+
+        self.callback_params['interaction'] = interaction
+
+        return await self.callback_func(**self.callback_params)
 
 class ButtonView(discord.ui.View):
 
@@ -24,11 +21,11 @@ class ButtonView(discord.ui.View):
         self.edit_func = edit_func
         self.context_i = context_i
         self.context_num = context_num
-        self.test_button = CustomButton(self.next_button, label="Test", style = discord.ButtonStyle.blurple)
+        self.test_button = CustomButton(callback_func=self.next_button, label="Test", style=discord.ButtonStyle.blurple, callback_params={'j': 5, 'i': 6})
 
         self.add_item(item = self.test_button)
 
-    async def prev_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def prev_button(self, interaction: discord.Interaction):
 
         contexts_len = len(self.contexts)
 
@@ -40,9 +37,11 @@ class ButtonView(discord.ui.View):
 
             self.context_i -= 1
 
-    async def next_button(self, interaction: discord.Interaction):
+    async def next_button(self, interaction: discord.Interaction, j, i):
 
         contexts_len = len(self.contexts)
+
+        print(j + i)
 
         if self.is_valid_index(self.context_i + 1, contexts_len):
             context = self.contexts[self.context_i + 1]
