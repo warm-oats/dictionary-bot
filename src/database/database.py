2df8fe0cf2eedee0231ddc0d814499e15eecb350
warm_oats <61860115+warm-oats@users.noreply.github.com>
2025-11-03 17:06:25 -0800

    Add deck query validation

--- src/database/database.py
+++ src/database/database.py
@@ -21,74 +21,92 @@ class DataBase:
                             );
                             """)
         
-        return self.cursor.fetchone()[0]
+        if (not self.cursor.fetchone()[0]):
+            raise ValueError(f"Deck '{deck_name}' does not exist")
 
     def create_deck(self, user_id: int, deck_name: str):
-        if (not self.deck_exists(user_id, deck_name)):
+        
+        try:
+            self.deck_exists(user_id, deck_name)
+
             self.cursor.execute(f"""
                                 INSERT into flashcards.user_decks(user_id, deck_name)
                                 VALUES({user_id}, '{deck_name}');
                                 """)
 
             self.connection.commit()
-        else:
-            raise ValueError(f"Deck '{deck_name}' already exists")
+        except ValueError as e:
+            print(e)
         
     def delete_deck(self, user_id, deck_name):
 
-        if (not self.deck_exists(user_id, deck_name)):
-            raise ValueError(name = f"Deck '{deck_name}' doesn't exist")
+        try:
+            self.deck_exists(user_id, deck_name)
 
-        self.cursor.execute(f""" 
-            DELETE FROM flashcards.user_decks
-            WHERE user_id = {user_id} 
-            AND deck_name = '{deck_name}';
-            """)
+            self.cursor.execute(f""" 
+                DELETE FROM flashcards.user_decks
+                WHERE user_id = {user_id} 
+                AND deck_name = '{deck_name}';
+                """)
+            
+            self.cursor.execute(f"""
+                DELETE FROM flashcards.flashcards
+                WHERE user_id = {user_id}
+                AND deck_name = '{deck_name}';
+                """)
         
-        self.cursor.execute(f"""
-            DELETE FROM flashcards.flashcards
-            WHERE user_id = {user_id}
-            AND deck_name = '{deck_name}';
-            """)
+            self.connection.commit()
+        except ValueError as e:
+            print(e)
 
     def update_deck_name(self, user_id, deck_name, new_deck_name):
-            
-        if (not self.deck_exists):
-            raise ValueError(name = f"Deck '{deck_name}' does not exist")
-
-        self.cursor.execute(f"""
-                            UPDATE flashcards.user_decks
-                            SET deck_name = '{new_deck_name}'
-                            WHERE user_id = {user_id}
-                            AND deck_name = '{deck_name}';
-                            """)
         
-        self.cursor.execute(f"""
-                            UPDATE flashcards.flashcards
-                            SET deck_name = '{new_deck_name}'
-                            WHERE user_id = {user_id}
-                            AND deck_name = '{deck_name}';
-                            """)
+        try:
+            self.deck_exists(user_id, deck_name)
+
+            self.cursor.execute(f"""
+                                UPDATE flashcards.user_decks
+                                SET deck_name = '{new_deck_name}'
+                                WHERE user_id = {user_id}
+                                AND deck_name = '{deck_name}';
+                                """)
+            
+            self.cursor.execute(f"""
+                                UPDATE flashcards.flashcards
+                                SET deck_name = '{new_deck_name}'
+                                WHERE user_id = {user_id}
+                                AND deck_name = '{deck_name}';
+                                """)
+            
+            self.connection.commit()
+        except ValueError as e:
+            print(e)
 
     def add_word(self, user_id, word, definition, deck_name):
-        if (self.deck_exists(user_id, deck_name)):
+        
+        try:
+            self.deck_exists(user_id, deck_name)
+
             self.cursor.execute(f"""
                                 INSERT into flashcards.flashcards(user_id, deck_name, front_page, back_page) 
                                 VALUES({user_id}, '{deck_name}', '{word}', '{definition}');
                                 """)
-        else:
-            raise ValueError(name = f"Invalid user id or deck name (does not exist)")
+        except ValueError as e:
+            print(e)
         
     def delete_word(self, user_id, word, deck_name):
+
         try:
+            self.deck_exists(user_id, deck_name)
+
             self.cursor.execute(f"""
                                 DELETE FROM flashcards.flashcards 
                                 WHERE user_id = {user_id}
                                 AND front_page = '{word}'
                                 AND deck_name = '{deck_name}';
                                 """)
-        except ValueError:
-            return f"Invalid user id or deck name (does not exist)"
+        except ValueError as e:
+            print(e)
         
     def update_word(self, user_id, new_word, new_definition, deck_name):
         pass
@@ -113,10 +131,10 @@ if __name__ == "__main__":
     db = DataBase()
 
     db.fetch_word()
-    db.update_deck_name(123456, "kOREAN fwaefafe", "Korean Stuff")
+    db.update_deck_name(123456, "kOREAN stuff", "Korean Stuff")
     print('---------------------')
     db.fetch_word()
-    db.add_word(123456, "hello", "Means hello", "Korean Stuff")
+    db.add_word(123456, "what", "Means hello", "Korean Stuff")
     print('---------------------')
     db.fetch_word()
     db.delete_word(123456, "hello", "Korean Stuff")
